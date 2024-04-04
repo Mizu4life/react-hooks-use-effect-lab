e040e06a028acef19a1930c4df868dbcb5d05471
Zainab Alnajjar <eng.zainab.alnajar@gmail.com>
2024-04-04 11:51:49 +0300

    Completed assignment!

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
 
+  // //////// 1- setTimeOut method:
+  // //setTimeout is a closure, therefore, when setTimeout is scheduled it uses the value of count at that exact moment in time
+  // //to solve the issue of using setTimeOut with state: (we don't need it in this case, but will need it if we want to update timeRemaining while using setTimeOut)
+  const timeRef= useRef(timeRemaining);
+  timeRef.current  = timeRemaining; 
+
+  useEffect(() => {
+    const timer = setTimeout(() => setTimeRemaining(timeRef.current - 1), 1000)
+    return () => clearTimeout(timer);
+  },[onAnswered, timeRemaining]);
+
+  
+  // ////// 2- setInterval method
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setTimeRemaining(timeRemaining - 1);
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, [onAnswered, timeRemaining]);
+
+  if(timeRemaining <= 0){
+    onAnswered(false);
+    setTimeRemaining(10);
+  }
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
+  
   const { id, prompt, answers, correctIndex } = question;
 
   return (
